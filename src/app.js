c7648962a6b1e74c68178361078210c7fb4c1acf
YERIM <dnaer456@gmail.com>
2020-08-24 22:08:36 +0900

    firebase title response

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import bodyParser from "body-parser";
@@ -16,6 +16,22 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.get("/", async (req, res) => {
+  console.log("call strat server");
+
+  try {
+    const fsRef = await firestore.collection("Boards");
+
+    await fsRef.get().then((response) =>
+      response.forEach((doc) => {
+        console.log(doc.data().title);
+      })
+    );
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server start http://localhost:${PORT}`);
